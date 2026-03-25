170050ffba505b4cfdbb1c54b6c528594d8d3f3e
Robert Heaney <rheaney@gmail.com>
2026-03-25 18:08:54 -0400

    Updated nav-links to fix build error

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -9,7 +9,15 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
  
-// ...
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
  
 export default function NavLinks() {
   const pathname = usePathname();
@@ -18,6 +26,10 @@ export default function NavLinks() {
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
+        const isActive =
+          pathname === link.href ||
+          (link.href !== '/dashboard' && pathname.startsWith(`${link.href}/`));
+
         return (
           <Link
             key={link.name}
@@ -25,7 +37,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-100 text-blue-600': isActive,
               },
             )}
           >

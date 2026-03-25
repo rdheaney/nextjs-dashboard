57b4e1569dcb219ac8d865eca1ade9691e5df242
Robert Heaney <rheaney@gmail.com>
2026-03-25 17:38:48 -0400

    Initial commit

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,37 +1,23 @@
 'use client';
-
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
+ 
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-];
-
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+ 
+// ...
+ 
 export default function NavLinks() {
   const pathname = usePathname();
-
+ 
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
-        const isActive =
-          pathname === link.href ||
-          (link.href !== '/dashboard' && pathname.startsWith(`${link.href}/`));
-
         return (
           <Link
             key={link.name}
@@ -39,7 +25,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': isActive,
+                'bg-sky-100 text-blue-600': pathname === link.href,
               },
             )}
           >
